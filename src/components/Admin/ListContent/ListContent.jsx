b97da38d38bb31fd9bc58f1369f03e2c4005e3fa
Fahim ahmed <88280947+Fahimahmed71@users.noreply.github.com>
2022-12-30 17:33:17 +0600

    commit

---
+++ src/components/Admin/ListContent/ListContent.jsx
@@ -0,0 +1,70 @@
+import axios from "axios";
+import React from "react";
+import { useState } from "react";
+import { MdDelete } from "react-icons/md";
+import { useQuery } from "react-query";
+
+const ListContent = () => {
+  const [services, setServices] = useState([]);
+
+  const { isLoading, refetch } = useQuery("listcontent", () =>
+    axios.get("http://localhost:3000/services").then((res) => {
+      setServices(res.data);
+    })
+  );
+
+  if (isLoading) {
+    return;
+  }
+
+  const handleDelete = (id) => {
+    const confirm = window.confirm("Are you sure?");
+
+    if (confirm) {
+      const url = `http://localhost:3000/services/${id}`;
+
+      axios.delete(url).then((res) => {
+        console.log(res.data);
+        refetch;
+      });
+    }
+  };
+
+  return (
+    <section>
+      <div className="overflow-x-auto">
+        <table className="table table-zebra w-full">
+          {/* <!-- head --> */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Location</th>
+              <th>Price</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* <!-- row 1 --> */}
+            {services.map((service, index) => (
+              <tr key={service._id}>
+                <th>{index + 1}</th>
+                <td>{service.hotelName}</td>
+                <td>{service.location}</td>
+                <td>{service.price} BDT</td>
+                <td>
+                  <MdDelete
+                    onClick={() => handleDelete(service._id)}
+                    className="text-2xl hover:text-red-500 cursor-pointer"
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </section>
+  );
+};
+
+export default ListContent;
---
+++ src/components/Shared/Loading/Loading.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <img
+      className="mx-auto block w-full 
+    "
+      src="https://img.pikbest.com/png-images/20190918/cartoon-snail-loading-loading-gif-animation_2734139.png!bw700"
+      alt="loading"
+    />
+  );
+};
+
+export default Loading;
